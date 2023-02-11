6010e181eadd47e50f7461ad3e17af0e75d65371
sathwikk6 <114384001+sathwikk6@users.noreply.github.com>
2023-02-11 20:45:00 +0530

    Add files via upload

---
+++ day1 palindrome.py
@@ -0,0 +1,11 @@
+#Given an integer x, return true if x is palindrome integer.
+string1= input("Enter the integer: ")
+string2=string1[::-1]
+if string1==string2:
+    print("True")
+else:
+    print("False")
+
+#Output:
+#Enter the integer: -121
+#False
