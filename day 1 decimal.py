a9c87a3139f2de9b9a7f85d5ae50367ab24b0ec5
sathwikk6 <114384001+sathwikk6@users.noreply.github.com>
2023-02-11 21:01:59 +0530

    Add files via upload

---
+++ day 1 decimal.py
@@ -0,0 +1,26 @@
+#A valid number can be split up into these components (in order):
+#1. A decimal number or an integer.
+#2. (Optional) An 'e' or 'E', followed by an integer.
+def isNumber(s):
+    valid_patterns = ('xd','x.d','xd.','xd.d','xsd','xsd.',
+              'xsd.d','xded','x.ded','xd.ed','xd.ded',
+              'xsded','xsd.ed','xsd.ded', 'xdesd', 'x.desd', 
+              'xd.esd', 'xd.desd', 'xsdesd', 'xsd.esd', 
+              'xsd.desd', 'xs.d', 'xs.ded', 'xs.desd'
+              )
+    patttern = 'x'
+    for ch in s:
+        if ch == '+' or ch == '-': patttern += 's'
+        elif ch == 'e' or ch == 'E': patttern += 'e'
+        elif ch.isdigit():
+            if patttern[-1] != 'd': patttern += 'd'
+        elif ch == '.': patttern += '.'
+        else: return False
+        
+    return patttern in valid_patterns
+str1=input("String=")
+print(isNumber(str1))
+
+#output:
+#String=2.90
+#True
